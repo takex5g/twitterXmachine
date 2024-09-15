ffc49e0fba9aecdc22bd29173217bb8192affdad
takex5g <takex5g@gmail.com>
2024-09-16 00:33:28 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vite'
-import fs from 'fs'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  //   plugins: [reactRefresh()],
-  server: {
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem'),
-    },
-  },
   base: '/twitterX',
 })
